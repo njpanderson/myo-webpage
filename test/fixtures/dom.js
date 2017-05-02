055baf68531da8cddc76352ca72de52056759f82
Neil Anderson <njp.anderson@icloud.com>
2017-05-02 13:21:49 +0100

    comment jsdom usage

--- test/fixtures/dom.js
+++ test/fixtures/dom.js
@@ -5,11 +5,13 @@ import { jsdom } from 'jsdom';
 module.exports = function(filename, finish) {
 	var markup = fs.readFileSync(path.resolve(filename));
 
+	// create a document instance and the usual globals
 	global.document = jsdom(markup || '');
 	global.window = document.defaultView;
 	global.navigator = window.navigator;
 
 	if (typeof finish === 'function') {
+		// fire the callback, sending it the above variables
 		finish.apply(this, [document, window, navigator]);
 	}
 };
