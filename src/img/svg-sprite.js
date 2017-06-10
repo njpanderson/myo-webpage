e851fb79a60838cc684c23dbd7f1821e933aacb1
Neil Anderson <njp.anderson@icloud.com>
2017-06-10 15:49:50 +0100

    Convert SVG attachment into a function

--- src/img/svg-sprite.js
+++ src/img/svg-sprite.js
@@ -3,36 +3,43 @@ import sprite from '../../dist/svg-sprite.json';
 const document = window.document || null,
 	glyph_re = new RegExp('-', 'g');
 
-var GLYPHS = {};
+var GLYPHS = {
+	length: 0
+};
 
-if (document) {
-	// add svg sprite data on dom load
-	document.addEventListener('DOMContentLoaded', function() {
-		var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+export function attachSVG() {
+	if (document) {
+		// add svg sprite data on dom load
+		document.addEventListener('DOMContentLoaded', function() {
+			var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 
-		// set styles to hide
-		svg.style.position = 'absolute';
-		svg.style.width = 0;
-		svg.style.height = 0;
+			// set styles to hide
+			svg.style.position = 'absolute';
+			svg.style.width = 0;
+			svg.style.height = 0;
 
-		// set innerHTML for svg data
-		svg.innerHTML = sprite.svg;
+			// set innerHTML for svg data
+			svg.innerHTML = sprite.svg;
 
-		// inject svg into top of document
-		document.body.insertBefore(svg, document.body.firstChild);
+			// inject svg into top of document
+			document.body.insertBefore(svg, document.body.firstChild);
 
-		// clean up
-		sprite.svg = null;
-	});
+			// clean up
+			sprite.svg = null;
+		});
+	}
 }
 
-// loop through glyphs and create exportable object
-sprite.glyphs.forEach((glyph) => {
-	var key = (glyph.toString().slice(0, -7).toUpperCase());
+if (!GLYPHS.length) {
+	// loop through glyphs and create exportable object
+	sprite.glyphs.forEach((glyph) => {
+		var key = (glyph.toString().slice(0, -7).toUpperCase());
 
-	key = key.replace(glyph_re, '_');
+		key = key.replace(glyph_re, '_');
 
-	GLYPHS[key] = '#' + glyph;
-});
+		GLYPHS[key] = '#' + glyph;
+		GLYPHS.length += 1;
+	});
+}
 
 export default GLYPHS;
